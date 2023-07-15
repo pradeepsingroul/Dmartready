626b51c9ca6e638e6e04fd49ffe4d7a1362f55ab
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-15 18:05:52 +0530

    DmartReady initial stage

---
+++ DmartReady_Project/src/main/java/com/Dmartready/DmartReadyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Dmartready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmartReadyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmartReadyProjectApplication.class, args);
+	}
+
+}
---
+++ DmartReady_Project/src/test/java/com/Dmartready/DmartReadyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dmartready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmartReadyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
