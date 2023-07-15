4b85f2ec91cc288247b21e8b416c8f09deafd7b6
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-16 00:43:46 +0530

    Stock repository

--- DmartReady_Project/src/main/java/com/Dmartready/Repository/StocksRepository.java
+++ DmartReady_Project/src/main/java/com/Dmartready/Repository/StocksRepository.java
@@ -12,7 +12,7 @@ import com.Dmartready.Models.Stocks;
 @Repository
 public interface StocksRepository extends JpaRepository<Stocks, Integer>{
 	
-	public Stocks findByName(String name) throws Stock_Exceptions;
+	public List<Stocks> findByName(String name) throws Stock_Exceptions;
 	
 	@Query("from com.Dmartready.Models.Stocks s where s.quantity >= ?1")
 	public List<Stocks> findStocksByQuantity(int quantity) throws Stock_Exceptions;
