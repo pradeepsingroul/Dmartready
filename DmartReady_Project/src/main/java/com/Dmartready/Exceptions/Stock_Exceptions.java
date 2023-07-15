73fb56cc501c9e12999b90848f72ba640f479094
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-15 18:37:00 +0530

    stock exceptiopns

---
+++ DmartReady_Project/src/main/java/com/Dmartready/Exceptions/Stock_Exceptions.java
@@ -0,0 +1,12 @@
+package com.Dmartready.Exceptions;
+
+public class Stock_Exceptions extends RuntimeException{
+
+	public Stock_Exceptions(){
+		
+	}
+	public Stock_Exceptions(String message) {
+		super(message);
+	}
+	
+}
