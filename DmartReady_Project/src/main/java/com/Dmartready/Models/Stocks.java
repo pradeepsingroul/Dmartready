773f92a31ea41c599217d69a69c88ca864552fc2
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-15 18:21:23 +0530

    Stocks Tables

---
+++ DmartReady_Project/src/main/java/com/Dmartready/Models/Stocks.java
@@ -0,0 +1,28 @@
+package com.Dmartready.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Stocks {
+
+	@Id
+	private Integer stockID;
+	
+	private String name;
+	
+	private String description;
+	
+	private Integer Quantity;
+	
+}
