8481cbf24b492b45db56f121b1da674057b00e85
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-16 00:44:30 +0530

    Stock controller completed

--- DmartReady_Project/src/main/java/com/Dmartready/Controller/StockController.java
+++ DmartReady_Project/src/main/java/com/Dmartready/Controller/StockController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Dmartready.Models.Stocks;
@@ -22,7 +24,7 @@ public class StockController {
 	private StocksService stockService;
 	
 	
-	@GetMapping("/")
+	@GetMapping("")
 	public ResponseEntity<List<Stocks>> getAllStocksController(){
 		
 		List<Stocks> listOfStock = stockService.getAllStocks();
@@ -30,7 +32,7 @@ public class StockController {
 		
 	}
 	
-	@GetMapping("/add")
+	@PostMapping("/add")
 	public ResponseEntity<Stocks> addStocksInSystem(@RequestBody Stocks  s){
 		
 		Stocks stock = stockService.addStock(s);
@@ -38,6 +40,37 @@ public class StockController {
 		
 	}
 	
+	@GetMapping("/")
+	public ResponseEntity<List<Stocks>> findByNameController(@RequestParam(name = "Name") String  name){
+		
+		List<Stocks> stock = stockService.findByNameService(name);
+		return new ResponseEntity<List<Stocks>>(stock,HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/update")
+	public ResponseEntity<Stocks> updateQuantity(@RequestParam(name = "id")int id, @RequestParam(name = "quantity")int quantity){
+		
+		Stocks stock = stockService.updateQuantityById(id, quantity);
+		return new ResponseEntity<Stocks>(stock,HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/greater")
+	public ResponseEntity<List<Stocks>> findBYQuantityGreater(@RequestParam(name = "quantity")int quantity){
+		
+		List<Stocks> stock = stockService.findByQuantityGreater(quantity);
+		return new ResponseEntity<List<Stocks>>(stock,HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/delete")
+	public ResponseEntity<Stocks> deleteById(@RequestParam(name = "id")int id){
+		
+		Stocks stock = stockService.deleteById(id);
+		return new ResponseEntity<Stocks>(stock,HttpStatus.OK);
+		
+	}
 	
 	
 }
