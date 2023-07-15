a413c919efdbe7ed1b70e6edf29eecbd535f248c
predeep kumar singroul <prasingroul4899@gmail.com>
2023-07-15 18:38:29 +0530

    store location Exception

---
+++ DmartReady_Project/src/main/java/com/Dmartready/Exceptions/Store_Location_Exception.java
@@ -0,0 +1,12 @@
+package com.Dmartready.Exceptions;
+
+public class Store_Location_Exception extends RuntimeException{
+
+	public Store_Location_Exception() {
+		
+	}
+	public Store_Location_Exception(String message) {
+		super(message)
+	}
+	
+}
